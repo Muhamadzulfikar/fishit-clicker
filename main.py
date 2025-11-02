7cc6c13cb092ff785637eaf344606720426d9ba3
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2025-11-02 10:54:33 +0700

    Create Fishit clicker script

---
+++ main.py
@@ -0,0 +1,61 @@
+from pynput.mouse import Button, Controller
+from pynput import keyboard, mouse as mouse_module
+import time
+import threading
+
+mouse = Controller()
+isSpeedClick = False
+isAutoPerfect = False
+
+
+def perfectMiddleMouse(x, y, button, pressed):
+    if button == Button.middle and pressed:
+        threading.Thread(target=lambda: (
+            mouse.press(Button.left),
+            time.sleep(0.3),
+            mouse.release(Button.left)
+        ), daemon=True).start()
+
+
+def autoPerfect():
+    global isAutoPerfect
+    while isAutoPerfect:
+        mouse.press(Button.left)
+        time.sleep(0.3)
+        mouse.release(Button.left)
+
+
+def speedClick():
+    global isSpeedClick
+    while isSpeedClick:
+        mouse.click(Button.left, 1)
+        time.sleep(0.1)
+
+
+def keyBinds(key):
+    global isAutoPerfect, isSpeedClick
+
+    try:
+        if key.char == 'f':
+            isAutoPerfect = not isAutoPerfect
+            if isAutoPerfect:
+                print("Auto Perfect ON")
+                threading.Thread(target=autoPerfect, daemon=True).start()
+            else:
+                print("Auto Perfect OFF")
+
+        elif key.char == 'j':
+            isSpeedClick = not isSpeedClick
+            if isSpeedClick:
+                print("Speed Click ON")
+                threading.Thread(target=speedClick, daemon=True).start()
+            else:
+                print("Speed Click OFF")
+    except AttributeError:
+        pass
+
+
+with mouse_module.Listener(on_click=perfectMiddleMouse) as mouse_listener, \
+     keyboard.Listener(on_press=keyBinds) as keyboard_listener:
+    mouse_listener.join()
+    keyboard_listener.join()
